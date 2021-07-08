3318bbd58bfb5e84638524b9c1b1a057b01b1ff3
Sagalay <sagalay93@gmail.com>
2021-07-08 20:37:51 +0300

    Lesson 15, update project with movies

--- Project/script.js
+++ Project/script.js
@@ -1,6 +1,10 @@
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+while (!numberOfFilms) {
+  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+}
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -10,10 +14,32 @@ const personalMovieDB = {
   privat: false,
 };
 
-const questionMovie = prompt('Один из последних просмотренных фильмов?', ''),
-  questionRate = prompt('На сколько оцените его?'),
-  questionMovie2 = prompt('Один из последних просмотренных фильмов?'),
-  questionRate2 = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+  const questionMovie = prompt('Один из последних просмотренных фильмов?', ''),
+    questionRate = prompt('На сколько оцените его?');
+  if (
+    questionMovie === '' ||
+    questionMovie === null ||
+    questionMovie.length > 50 ||
+    questionRate === '' ||
+    questionRate === null ||
+    questionRate.length > 50
+  ) {
+    alert('Ошибка, пожалуйста, ответь на вопрос');
+    i--;
+  } else {
+    personalMovieDB.movies[questionMovie] = questionRate;
+  }
+}
 
-personalMovieDB.movies[questionMovie] = questionRate;
-personalMovieDB.movies[questionMovie2] = questionRate2;
+if (personalMovieDB.count < 10) {
+  alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count < 30 && personalMovieDB.count >= 10) {
+  alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+  alert('Вы киноман');
+} else {
+  alert('Что-то пошло не так!');
+}
+
+console.log(personalMovieDB);
