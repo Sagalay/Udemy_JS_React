41ebe3e08647cd89bfa0533695f2ecafa69fbf65
Sagalay <sagalay93@gmail.com>
2021-07-14 00:27:10 +0300

    Lesson 20: Objects and destructuring completed

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
 'use strict';
 
-function first() {
-  // Do something
-  setTimeout(() => {
-    console.log(1);
-  }, 500);
+const options = {
+  name: 'test',
+  width: 1024,
+  height: 1024,
+  colors: {
+    borderColor: 'black',
+    background: 'red',
+  },
+  makeTest() {
+    console.log('Test');
+  },
+};
+
+const { borderColor: border, background: bg, borderWidth = '50px' } = options.colors;
+console.log(border);
+console.log(bg);
+console.log(parseInt(borderWidth));
+
+console.log(options.name);
+
+delete options.name;
+
+console.log(options);
+
+let counter = 0;
+for (let key in options) {
+  if (typeof(options[key]) === 'object') {
+    for (const i in options[key]) {
+      console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+    }
+  } else {
+    console.log(`Свойство ${key} имеет значение ${options[key]}`);
+  }
+  counter++;
 }
+console.log(counter);
 
-function second() {
-  console.log(2);
-}
-
-first();
-second();
-
-function learnJS(lang, callback) {
-  console.log(`Я учу: ${lang}`);
-  callback();
-}
-
-function done() {
-  console.log('Я прошёл этот урок!');
-}
-
-learnJS('Javascript', done);
+console.log(Object.keys(options).length);
