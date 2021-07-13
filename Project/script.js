7c1383ab5cd02c60d7e240327a9449718eb40a2f
Sagalay <sagalay93@gmail.com>
2021-07-13 20:43:18 +0300

    Lesson 18: add funtions in project Movie

--- Project/script.js
+++ Project/script.js
@@ -1,10 +1,16 @@
 'use strict';
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
 
-while (!numberOfFilms) {
+function start() {
   numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+  while (!numberOfFilms) {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  }
 }
+
+start();
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -14,32 +20,53 @@ const personalMovieDB = {
   privat: false,
 };
 
-for (let i = 0; i < 2; i++) {
-  const questionMovie = prompt('Один из последних просмотренных фильмов?', ''),
-    questionRate = prompt('На сколько оцените его?');
-  if (
-    questionMovie === '' ||
-    questionMovie === null ||
-    questionMovie.length > 50 ||
-    questionRate === '' ||
-    questionRate === null ||
-    questionRate.length > 50
-  ) {
-    alert('Ошибка, пожалуйста, ответь на вопрос');
-    i--;
-  } else {
-    personalMovieDB.movies[questionMovie] = questionRate;
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const questionMovie = prompt(
+        'Один из последних просмотренных фильмов?',
+        ''
+      ),
+      questionRate = prompt('На сколько оцените его?');
+    if (
+      questionMovie === '' ||
+      questionMovie === null ||
+      questionMovie.length > 50 ||
+      questionRate === '' ||
+      questionRate === null ||
+      questionRate.length > 50
+    ) {
+      alert('Ошибка, пожалуйста, ответь на вопрос');
+      i--;
+    } else {
+      personalMovieDB.movies[questionMovie] = questionRate;
+    }
   }
 }
 
-if (personalMovieDB.count < 10) {
-  alert('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count < 30 && personalMovieDB.count >= 10) {
-  alert('Вы классический зритель');
-} else if (personalMovieDB.count >= 30) {
-  alert('Вы киноман');
-} else {
-  alert('Что-то пошло не так!');
+rememberMyFilms();
+
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+  } else if (personalMovieDB.count < 30 && personalMovieDB.count >= 10) {
+    alert('Вы классический зритель');
+  } else if (personalMovieDB.count >= 30) {
+    alert('Вы киноман');
+  } else {
+    alert('Что-то пошло не так!');
+  }
 }
 
-console.log(personalMovieDB);
+detectPersonalLevel();
+
+function writeYourGenres() {
+  for (let i = 0; i < 3; i++) {
+    personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}`);
+  }
+}
+writeYourGenres();
+
+function showMyDB(hidden) {
+  hidden ? console.log('Данные приватны!') : console.log(hidden);
+}
+showMyDB(personalMovieDB);
--- script.js
+++ script.js
@@ -10,23 +10,23 @@
 // console.log(str.toUpperCase());
 // console.log(str.toLowerCase());
 
-// const fruit = 'Some fruit';
+const fruit = 'Some fruit';
 
-// console.log(fruit.indexOf('fr'));
+console.log(fruit.indexOf('fr'));
 
-// const logg = 'Hello world!!!';
+const logg = 'Hello world!!!';
 
-// console.log(logg.slice(6));
+console.log(logg.slice(6));
 
-// console.log(logg.slice(6, 11));
+console.log(logg.slice(6, 11));
 
-// console.log(logg.substring(6, 11));
+console.log(logg.substring(6, 11));
 
-// console.log(logg.substr(6, 5));
+console.log(logg.substr(6, 5));
 
-// const num = 12.2;
-// console.log(Math.round(num));
+const num = 12.2;
+console.log(Math.round(num));
 
-// const test = "12.2px";
-// console.log(parseInt(test));
-// console.log(parseFloat(test));
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
