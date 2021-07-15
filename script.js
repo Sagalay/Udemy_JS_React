3f2023daa1604cf2b61260579d57167920d76399
Sagalay <sagalay93@gmail.com>
2021-07-15 23:53:26 +0300

    Lesson 22: Object link and Spread

--- script.js
+++ script.js
@@ -1,36 +1,95 @@
 'use strict';
 
-const arr = [1, 22, 13, 4, 75];
+// let a = 5,
+//   b = a;
 
-console.log(arr.sort(compareNum));
+// b = b + 5;
 
-function compareNum(a, b) {
-  return a - b;
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//   a: 5,
+//   b: 1
+// };
+
+// const copy = obj;
+
+// copy.a = 10;
+
+// console.log(obj);
+// console.log(copy);
+
+// function copy(mainObj) {
+//   let objCopy = {};
+
+//   for (let key in mainObj) {
+//     objCopy[key] = mainObj[key];
+//   }
+//   return objCopy;
+// }
+
+// const numbers = {
+//   a: 2,
+//   b: 5,
+//   c: {
+//     x: 7,
+//     y: 4,
+//   },
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10;
+// newNumbers.c.x = 10;
+
+// console.log(numbers);
+// console.log(newNumbers);
+
+const obj = {
+  a: 22,
+  b: 13,
+};
+
+const testObj = Object.assign({}, obj);
+
+testObj.a = 0;
+
+// console.log(obj);
+// console.log(testObj);
+
+// const oldArray = ['a', 'b', 'c', 'd'];
+// const newArray = oldArray.slice();
+// newArray[1] = 'qqqqqqwwwe';
+
+// console.log(oldArray);
+// console.log(newArray);
+
+// const video = ['youtube', 'vimeo', 'rutube'],
+//   blogs = ['wordpress', 'livejournal', 'blogger'],
+//   internet = [...video, '&&&' , ...blogs, 'vk'];
+
+// console.log(internet);
+
+function log(a, b, c) {
+  console.log(a);
+  console.log(b);
+  console.log(c);
 }
 
-// arr[99] = 0;
-// console.log(arr.length);
-// console.log(arr);
+const num = [2, 5, 7, 10];
 
-// arr.forEach(function(elem, i, arr) {
-//   console.log(`Елемент ${elem} с индексом: ${i} внутри массива ${arr}`);
-// });
+log(...num);
 
-// arr.pop();
-// arr.push(10);
+const array = ['a', 'b'];
 
-// console.log(arr);
+const newArr = [...array];
 
-// for (let i = 0; i < arr.length; i++) {
-//   console.log(arr[i]);
-// }
+const q = {
+  one: 1,
+  two: 2
+};
 
-// const str = 'Test'.split('').reverse().join('');
-// console.log(str);
-// for (let value of str) {
-//   console.log(value);
-// }
+const newObj = {...q};
 
-// for (let value of arr) {
-//   console.log(value);
-// }
+console.log(newObj);
