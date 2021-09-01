4a0d98eeeb0f016ad02fdf60c229cfd4f1f254c3
Sagalay <sagalay93@gmail.com>
2021-09-02 00:41:11 +0300

    Lesson 028: elements by page

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
 'use strict';
 
-// Какое будет выведено значение: let x = 5; alert( x++ ); ?
+const box = document.getElementById('box');
+console.log(box);
 
-// Чему равно такое выражение: [ ] + false - null + true ?
+const btns = document.getElementsByTagName('button');
+console.log(btns[3]);
 
-// Что выведет этот код: let y = 1; let x = y = 2; alert(x); ?
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
 
-// Чему равна сумма [ ] + 1 + 2?
+const hearts = document.querySelectorAll('.heart');
+hearts.forEach((item) => {
+  console.log(item);
+});
 
-// Что выведет этот код: alert( "1"[0] )?
-
-// Чему равно 2 && 1 && null && 0 && undefined ?
-
-// Есть ли разница между выражениями? !!( a && b ) и (a && b)?
-
-// Что выведет этот код: alert( null || 2 && 3 || 4 ); ?
-
-// a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ?
-
-// Что выведет этот код: alert( +"Infinity" ); ?
-
-// Верно ли сравнение: "Ёжик" > "яблоко"?
-
-// Чему равно 0 || "" || 2 || undefined || true || falsе ?
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
